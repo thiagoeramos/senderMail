a7813ebd82d1b8d15666da184ec73aa833d99589
thiagoeramos <thiagoevangelista.contato@gmail.com>
2014-10-09 10:39:37 -0300

    #reenviando emails

--- application/models/sender_model.php
+++ application/models/sender_model.php
@@ -18,7 +18,7 @@ class sender_model extends CI_Model{
 		
 			$this->db->from($this->tablename);
 	
-			$this->db->where(array('sent'=>'0','status_id'=>'1'));
+			$this->db->where(array('sent'=>'1','status_id'=>'1','id >='=>'10000'));
 			
 			$this->db->limit(500);
 	
